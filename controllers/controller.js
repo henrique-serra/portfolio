539e0217521654dccdb895b6d159b96591d4b8a2
henrique-serra <66788126+henrique-serra@users.noreply.github.com>
2025-09-08 13:18:11 +0000

    checkpoint

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,11 +1,11 @@
 export const index = function index(req, res) {
-    res.render('index');
+    res.render('index', { title: 'Home' });
 };
 
 export const about = function about(req, res) {
-    res.render('about');
+    res.render('about', { title: 'About' });
 };
 
 export const notFound = function notFound(req, res) {
-    res.status(404).render('404');
+    res.status(404).render('404', { title: 'Not Found' });
 };
